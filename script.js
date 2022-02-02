157df61c5a1ef69e9a72da4f51cde2d02b74ff1c
Halyna <galina14b@gmail.com>
2022-02-02 14:17:36 +0200

    add files

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed: 400,
+  spaceBetween: 100,
+  slidesPerView: 3,
+  spaceBetween: 45,
+
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
